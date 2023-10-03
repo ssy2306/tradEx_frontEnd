655c70ad636238f6de0776dd3fc069f67876a108
Shahil Singh Yadav <77876285+ssy2306@users.noreply.github.com>
2023-10-03 19:05:40 +0530

    start

---
+++ public-tradex/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './logo.png'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
