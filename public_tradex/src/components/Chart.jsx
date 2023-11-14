941cfbf1f91a73b4ddb1c1328ac674cedcbdd703
Shahil Singh Yadav <77876285+ssy2306@users.noreply.github.com>
2023-11-14 19:20:19 +0530

    CHART UPDATED

--- public_tradex/src/components/Chart.jsx
+++ public_tradex/src/components/Chart.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Typography } from '@mui/material';
 import TopVolumesTable from './TopVolumesTable';
-
+import { Line } from 'react-chartjs-2';
 
 const Chart = () => {
     // Use the `useParams` hook to access the `id` parameter from the URL
     const { id } = useParams();
+    const [chartData, setChartData] = useState(null);
     const [cryptoData, setCryptoData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const chartRef = useRef(null);
 
     useEffect(() => {
         const fetchCryptoData = async () => {
@@ -21,6 +23,14 @@ const Chart = () => {
                 const data = await response.json();
                 setCryptoData(data);
                 console.log(data);
+
+                const chartResponse = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=inr&days=1`);
+                if (!chartResponse.ok) {
+                  throw new Error(`Chart request failed with status ${chartResponse.status}`);
+                }
+                const chartData = await chartResponse.json();
+                setChartData(chartData.prices);
+
                 setLoading(false);
             } catch (error) {
                 setError(error);
@@ -29,6 +39,11 @@ const Chart = () => {
         };
 
         fetchCryptoData();
+        return () => {
+            if (chartRef.current) {
+              chartRef.current.destroy();
+            }
+          };
     }, [id]);
 
     const getTopVolumes = () => {
@@ -49,16 +64,42 @@ const Chart = () => {
         return <div>Error: {error.message}</div>;
     }
 
-    if (!cryptoData) {
+    if (!cryptoData || !chartData) {
         return <div>No data available.</div>;
     }
     const descriptionHtml = { __html: cryptoData.description.en };
+    const isPositive = cryptoData.market_data.price_change_percentage_24h > 0;
+    const price_change_percentage_24h = isPositive ? `+ ${cryptoData.market_data.price_change_percentage_24h}%` : `${cryptoData.market_data.price_change_percentage_24h}%`;
+    const bgColor = isPositive ? 'linear-gradient(to bottom left, #073b3a 0%, #21d375 100%)': 'linear-gradient(to bottom left, #801818 0%, #960018 100%)';
+
+
+    // Extracting labels (timestamps) and data values from the chartData
+  const labels = chartData.map((data) => new Date(data[0]).toLocaleTimeString());
+  const dataValues = chartData.map((data) => data[1]);
+
+  // Chart.js data configuration
+  const chartConfig = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Price',
+        data: dataValues,
+        fill: false,
+        borderColor: 'rgba(75,192,192,1)',
+        tension: 0.1,
+      },
+    ],
+  };
 
     return (
         <Box sx={{
-            marginLeft: '270px',
+            marginLeft: '249px',
             paddingTop: '2rem',
-            paddingBottom: '2rem'
+            paddingLeft: '2rem',
+            paddingBottom: '2rem',
+            background: bgColor,
+            textDecoration: 'none',
+            height: '100%'
         }}>
             <Box sx={{
                 color: 'white',
@@ -73,23 +114,32 @@ const Chart = () => {
             }} >
                 Rank #{cryptoData.coingecko_rank}
             </Box>
+
             <Box sx={{
-                display: 'flex',
+                display: 'block',
                 padding: '1rem',
                 paddingLeft: 0,
                 alignItems: 'center'
             }}>
-                <img src={cryptoData.image.small} style={{ width: '60px', height: '60px', marginRight: 15, }} />
-                <p style={{ fontSize: '2rem', margin: 0, }}><b>{cryptoData.name}</b></p>
+                <p style={{ fontSize: '2rem', margin: 0, }}> {price_change_percentage_24h}</p><br /> 
+                Last updated - {cryptoData.last_updated}
+                <Box sx={{ display: 'flex'}}>
+                <img src={cryptoData.image.large} style={{ width: '60px', height: '60px', marginRight: 15, }} />
+              
+                <p style={{ fontSize: '2rem', margin: 0, }}><b>{cryptoData.name} {`(${cryptoData.symbol})`} </b></p>
+                 
+            </Box>
+            <Box>
+      {/* Your existing component code */}
+      {/* ... */}
+
+      {/* Display the chart */}
+      <Typography variant="h6">Price Chart (Last 24 Hours)</Typography>
+      <Line data={chartConfig} />
+    </Box>
             </Box>
             <b style={{ fontSize: '1.2rem'}}>Profile</b>
             <Typography variant="body1" dangerouslySetInnerHTML={descriptionHtml} />
-            <Typography variant="h6">Website</Typography>
-            <Typography variant="body1">
-                <a href={cryptoData.links.homepage[0]} target="_blank" rel="noopener noreferrer">
-                    meta.com
-                </a>
-            </Typography>
             <Typography variant="h6">Number of Users</Typography>
             <Typography variant="body1">{cryptoData.watchlist_portfolio_users}K</Typography>
             <Box sx={{
